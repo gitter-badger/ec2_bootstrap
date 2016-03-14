f5f0e9cf54035b7939074e0ce465e24f45963303
Rachel King <rachel@cozy.co>
2016-03-14 11:45:03 -0700

    Add EC2Bootstrap::Instance class.

--- lib/ec2_bootstrap.rb
+++ lib/ec2_bootstrap.rb
@@ -2,68 +2,29 @@ require 'yaml'
 require 'json'
 require 'open3'
 require 'ec2_bootstrap/version'
+require 'ec2_bootstrap/instance'
 
 class EC2Bootstrap
 
 	def initialize(config_path, dryrun=true)
-		@config = YAML.load(File.read(config_path))
-		@nodes = @config['nodes']
+		config = YAML.load(File.read(config_path))
+		@cloud_config = config['cloud_config']
+
+		instances_config = config['instances'].map {|i| i.map {|key, value| [key.to_sym, value]}.to_h}
+		@instances = instances_config.map {|n| Instance.new(n)}
 
 		@dryrun = dryrun
-	end
-
-	def build_knife_config(node)
-		knife_config = node['knife_ec2_flags']
-		node_name = node['node_name']
-
-		additional_knife_config = {
-			'node-name' => node_name,
-			'tags' => "Name=#{node_name}",
-		}
-
-		node['knife_ec2_flags'] = knife_config.merge(additional_knife_config)
-		return node
-	end
-
-	def format_knife_shell_command(knife_flags)
-		prefix = 'knife ec2 server create '
-		knife_flag_array = knife_flags.map {|key, value| ['--' + key, value]}.flatten.compact
-		return prefix + knife_flag_array.join(' ')
-	end
-
-	def generate_cloud_config(node)
-		cloud_config = @config['cloud_config']
-		node_name = node['node_name']
-
-		cloud_config['hostname'] = node_name
-		cloud_config['fqdn'] = "#{node_name}.#{node['domain']}"
-
-		formatted_cloud_config = cloud_config.to_yaml.gsub('---', '#cloud-config')
-		cloud_config_path = "cloud_config_#{node_name}.txt"
-
-		if @dryrun
-			puts "If this weren't a dry run, I would write the following contents to #{cloud_config_path}:"
-			puts formatted_cloud_config, "\n"
-		else
-			File.open(cloud_config_path, 'w') {|f| f.write(formatted_cloud_config)}
-			puts "Wrote cloud config to #{cloud_config_path}."
-		end
-
-		node['knife_ec2_flags']['user-data'] = cloud_config_path
-		return node
 	end
 
 	def create_instances
 		puts "This was a dry run. No EC2 instances were created.\n\n" if @dryrun
 
-		@nodes.each do |node|
-			puts "Node name: #{node['node_name']}"
+		@instances.each do |instance|
+			puts "Instance name: #{instance.name}"
 
-			node = self.build_knife_config(node)
+			instance.generate_cloud_config(@cloud_config, @dryrun) if @cloud_config
 
-			node = self.generate_cloud_config(node) if @config['cloud_config']
-
-			knife_shell_command = self.format_knife_shell_command(node['knife_ec2_flags'])
+			knife_shell_command = instance.format_knife_shell_command
 			puts 'Knife shell command:', knife_shell_command, "\n"
 			
 			unless @dryrun
---
+++ lib/ec2_bootstrap/instance.rb
@@ -0,0 +1,44 @@
+class EC2Bootstrap
+	class Instance
+
+		attr_accessor :name
+		attr_accessor :knife_ec2_flags
+
+		def initialize(instance_name:, domain:, knife_ec2_flags:)
+			@name = instance_name
+			@domain = domain
+
+			additional_knife_flags = {
+				'node-name' => @name,
+				'tags' => "Name=#{@name}"
+			}
+
+			@knife_ec2_flags = knife_ec2_flags.merge(additional_knife_flags)
+		end
+
+		def format_knife_shell_command
+			prefix = 'knife ec2 server create '
+			knife_flag_array = @knife_ec2_flags.map {|key, value| ['--' + key, value]}.flatten.compact
+			return prefix + knife_flag_array.join(' ')
+		end
+
+		def generate_cloud_config(cloud_config, dryrun)
+			cloud_config['hostname'] = @name
+			cloud_config['fqdn'] = "#{@name}.#{@domain}"
+
+			formatted_cloud_config = cloud_config.to_yaml.gsub('---', '#cloud-config')
+			cloud_config_path = "cloud_config_#{@name}.txt"
+
+			if dryrun
+				puts "If this weren't a dry run, I would write the following contents to #{cloud_config_path}:"
+				puts formatted_cloud_config, "\n"
+			else
+				File.open(cloud_config_path, 'w') {|f| f.write(formatted_cloud_config)}
+				puts "Wrote cloud config to #{cloud_config_path}."
+			end
+
+			@knife_ec2_flags['user-data'] = cloud_config_path
+		end
+
+	end
+end
